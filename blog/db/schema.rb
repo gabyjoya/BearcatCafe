50a9bf9dc46edb6a22b4e66989e5863bbe25badf
Gaby Joya <gjoya@uncc.edu>
2020-04-15 04:13:29 +0000

    Added menu model

--- blog/db/schema.rb
+++ blog/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_15_011103) do
+ActiveRecord::Schema.define(version: 2020_04_15_041207) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -28,6 +28,13 @@ ActiveRecord::Schema.define(version: 2020_04_15_011103) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  create_table "menus", force: :cascade do |t|
+    t.string "item"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "ratings", force: :cascade do |t|
